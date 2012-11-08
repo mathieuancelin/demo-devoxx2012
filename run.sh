e2ada95bb1d234ab5b86ab9630a5909d70a2da04
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2012-11-08 10:10:37 +0100

    Add overridable web res path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+[ -z "$DEVPATH" ] && DEVPATH='/Users/mathieuancelin/Desktop/devoxx2012/demo-devoxx-2012/nevernote-core/src/main/resources'
 cd ./container/target/container-1.2.0-SNAPSHOT-all/container-1.2.0-SNAPSHOT
-java -Ddevpath='/Users/mathieuancelin/Desktop/devoxx2012/demo-devoxx-2012/nevernote-core/src/main/resources' -jar bin/felix.jar 
+java -Ddevpath=$DEVPATH -jar bin/felix.jar 
